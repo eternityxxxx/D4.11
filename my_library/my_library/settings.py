1685f8203bc537a42a579ac2bf85a8ede3fe8a11
eternityxxxx <gpavlikov7@gmail.com>
2020-08-08 17:10:40 +0600

    Add models, load data-file

--- my_library/my_library/settings.py
+++ my_library/my_library/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'p_library', 
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
---
+++ my_library/p_library/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from django.utils.translation import gettext as _
+
+
+class Author(models.Model):
+    full_name = models.TextField(verbose_name=_("Имя автора"))
+    birth_year = models.PositiveSmallIntegerField(verbose_name=_("Год рождения"))
+    country = models.CharField(max_length=2, verbose_name=_("Страна"))
+
+    def __str__(self):
+        return self.full_name
+
+
+class Book(models.Model):
+    ISBN = models.CharField(max_length=13, verbose_name=_("Международный стандартный книжный номер"))
+    title = models.TextField(verbose_name=_("Название"))
+    description = models.TextField(verbose_name=_("Аннотация"))
+    year_release = models.PositiveSmallIntegerField(verbose_name=_("Год издания"))
+    copy_count = models.PositiveSmallIntegerField(verbose_name=_("Число копий"))
+    price = models.DecimalField(max_digits=12, decimal_places=2, verbose_name=_("Цена"))
+
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, verbose_name=_("Автор"), related_name="book_author")
+
+    def __str__(self):
+        return self.title
+
+
+class Publisher(models.Model):
+    name = models.TextField(verbose_name=_("Название"))
+    
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, verbose_name=_("Книга"), related_name="publisher_book")
+
+    def __str__(self):
+        return self.name
